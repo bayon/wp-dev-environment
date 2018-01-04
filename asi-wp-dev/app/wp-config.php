b8e11317ae809e4e7bd27947673430ac920fcb42
Bayon Forte <bayon.forte@ASI.local>
2018-01-04 14:51:45 -0500

    created a global var for react boilerplate that didnt work

--- asi-wp-dev/app/wp-config.php
+++ asi-wp-dev/app/wp-config.php
@@ -37,6 +37,10 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+
+define( 'WPRB_AJAX_BASE', 'http://localhost:3000/wp-json/wprb/v1' );
+
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
